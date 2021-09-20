a87336c588018c209816b01ae7d3be6ccf5938a3
superbakuryu <superbakuryu@gmail.com>
2021-09-20 11:16:34 +0700

    change

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 from flask import Flask
+import pymongo
+from datetime import datetime
+
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+mydb = myclient["mydatabase"]
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Mai Trọng Thuần"
+    NOW = datetime.now().timestamp()
+    mydb.visits.insert_one({'created_at': NOW})
+    num = mydb.visits.count_documents({})
+    return f"Số lượt truy cập: {num}"

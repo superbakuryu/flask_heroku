8054ad4c4c01de652a9928888eaecfd7fbe3d85b
superbakuryu <superbakuryu@gmail.com>
2021-09-20 11:25:50 +0700

    db fake

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
 from flask import Flask
-import pymongo
 from datetime import datetime
 
-myclient = pymongo.MongoClient("mongodb://0.0.0.0:27017/")
-mydb = myclient["thuan"]
 app = Flask(__name__)
+
+DB_FAKE = []
 
 @app.route("/")
 def hello():
     NOW = datetime.now().timestamp()
-    mydb.visits.insert_one({'created_at': NOW})
-    num = mydb.visits.count_documents({})
+    DB_FAKE.append({'created_at': NOW})
+    num = len(DB_FAKE)
     return f"Số lượt truy cập: {num}"

67b00b2e3cc101ebd24d3ecc0c68e3d2f13ffb2a
superbakuryu <superbakuryu@gmail.com>
2021-09-20 13:27:03 +0700

    add mongo cluster

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 from flask import Flask
 from datetime import datetime
-
+import pymongo
 app = Flask(__name__)
 
-DB_FAKE = []
+myclient = pymongo.MongoClient('mongodb+srv://thuan:thuan@cluster0.9gguq.mongodb.net/test')
+mydb = myclient["mydatabase"]
 
 @app.route("/")
 def hello():
     NOW = datetime.now().timestamp()
-    DB_FAKE.append({'created_at': NOW})
-    num = len(DB_FAKE)
+    mydb.visits.insert_one({'created_at': NOW})
+    num = mydb.visits.count_documents({})
     return f"Số lượt truy cập: {num}"
